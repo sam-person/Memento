1f9b175961a51c3f47f512b3817aa843e835d10e
Daniel Tan <danieltan843@gmail.com>
2016-07-11 16:22:57 +1000

    Added FP Player, plane, 2 cubes Added raycast code to player camera

---
+++ Memento/Assets/Scripts/DetectMouseOverObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectMouseOverObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Update()
+	{
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit))
+		{
+			print (hit.collider.name);
+		}
+	}
+}
