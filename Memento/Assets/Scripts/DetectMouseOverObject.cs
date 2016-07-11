a28f0762bbf404e892b8a1f3b95d1af6c2d221ec
Daniel Tan <danieltan843@gmail.com>
2016-07-11 19:43:44 +1000

    Added hud for hovering over key objects

--- Memento/Assets/Scripts/DetectMouseOverObject.cs
+++ Memento/Assets/Scripts/DetectMouseOverObject.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DetectMouseOverObject : MonoBehaviour {
+
+	public Image hud;
+	public Sprite normal;
+	public Sprite openHand;
+	public Sprite closeHand;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +22,16 @@ public class DetectMouseOverObject : MonoBehaviour {
 		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		if(Physics.Raycast(ray, out hit))
 		{
-			print (hit.collider.name);
+//			print (hit.collider.tag);
+			if (hit.collider.CompareTag ("KeyObject")) { // Check object's tag
+				if (Input.GetButton ("Fire1")) { // Check if player is clicking
+					hud.sprite = closeHand;
+				} else { // Player is JUST looking at object
+					hud.sprite = openHand;
+				}
+			} else { // Show cross hair instead
+				hud.sprite = normal;
+			}
 		}
 	}
 }
