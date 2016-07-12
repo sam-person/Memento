11d0090578bb5ee6bf6261be3e41cd3643cdb32f
Daniel Tan <danieltan843@gmail.com>
2016-07-12 14:05:46 +1000

    - Can now pick up object (hold down left click)

--- Memento/Assets/Scripts/DetectMouseOverObject.cs
+++ Memento/Assets/Scripts/DetectMouseOverObject.cs
@@ -9,9 +9,18 @@ public class DetectMouseOverObject : MonoBehaviour {
 	public Sprite openHand;
 	public Sprite closeHand;
 
+	public float distance;
+	public float smooth;
+
+	private bool isCarrying;
+
+	GameObject carriedObject;
+	Camera mainCamera;
+
 	// Use this for initialization
 	void Start () {
-		
+		isCarrying = false;
+		mainCamera = GetComponent<Camera> ();
 	}
 
 	Ray ray;
@@ -19,19 +28,70 @@ public class DetectMouseOverObject : MonoBehaviour {
 
 	void Update()
 	{
-		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out hit))
-		{
-//			print (hit.collider.tag);
+		// Check if player is carrying an object
+		if (isCarrying) {
+			Carry (carriedObject);
+			CheckDropObject (); // check if player wants to drop object
+		} else {
+			PickUp ();
+		}
+
+	}
+
+	// pick up target object
+	void PickUp() {
+		ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+		if(Physics.Raycast(ray, out hit)) {
 			if (hit.collider.CompareTag ("KeyObject")) { // Check object's tag
-				if (Input.GetButton ("Fire1")) { // Check if player is clicking
+				if (Input.GetButtonDown ("Fire1")) { // Check if player is clicking
 					hud.sprite = closeHand;
+					Rigidbody rbCarriedObject = hit.collider.GetComponent<Rigidbody> ();
+
+					if (rbCarriedObject != null) {
+						isCarrying = true;
+						carriedObject = rbCarriedObject.gameObject;
+						rbCarriedObject.useGravity = false;
+					}
 				} else { // Player is JUST looking at object
 					hud.sprite = openHand;
 				}
 			} else { // Show cross hair instead
 				hud.sprite = normal;
 			}
+		}
+
+	}
+
+	/// <summary>
+	/// Carry the specified heldObject.
+	/// </summary>
+	/// <param name="heldObject">The object to be carried.</param>
+	void Carry(GameObject heldObject) {
+		// Move object to middle of screen
+		heldObject.transform.position = 
+			Vector3.Lerp(heldObject.transform.position, 
+				mainCamera.transform.position + mainCamera.transform.forward * distance,
+				Time.deltaTime * smooth);
+
+		// prevent rotation
+		heldObject.transform.rotation = Quaternion.identity;
+	}
+
+	// Check if player wants to drop object by clicking while holding an object
+	void CheckDropObject() {
+		if (Input.GetButtonUp ("Fire1")) { // Check if player is clicking
+			isCarrying = false; // turn off carrying
+
+			// Turn Gravity on
+			Rigidbody rbCarriedObject = carriedObject.GetComponent<Rigidbody> ();
+			if (rbCarriedObject != null) { 
+				rbCarriedObject.useGravity = true;
+			}
+
+			rbCarriedObject.velocity = Vector3.zero; // reset velocity
+
+			carriedObject = null; // reset object
 		}
 	}
 }
